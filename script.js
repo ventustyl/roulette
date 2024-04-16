30272ac5d54e901037f51e4d5f8bb59d63e08081
Eric Venturino <eric.venturino@orange.fr>
2024-04-16 22:42:10 +0200

    maj total

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Assurez-vous d'avoir importé ShortUniqueId soit via un <script> tag ou un module
 let maintenance = document.querySelector(".maintenance");
 let listing = document.querySelector(".listing");
+let btnTotal = document.querySelector(".btnTotal");
+let score = document.querySelector(".score");
 let users = "",
   tache = "",
-  heure = "";
+  heure = "",
+  montant = "";
 
 async function fetchData(url) {
   try {
@@ -26,6 +29,7 @@ async function addRandomTask() {
     tache = dataTache.taches[randTache];
     users = dataUser.user[randUser];
     heure = Math.floor(Math.random() * 24 + 1) + "h";
+    montant = parseInt(heure) * 82.67 + "€";
     check();
   }
 }
@@ -35,7 +39,8 @@ function createTodoElement(key, value) {
   newTodo.classList.add("todo");
   newTodo.dataset.key = key; // Use data-key to relate DOM elements with localStorage keys
   let values = value.split(",");
-  newTodo.innerHTML = `<h3>${values[0]}</h3><p class="user">${values[1]} | ${values[2]}</p><hr>`;
+  console.log(values);
+  newTodo.innerHTML = `<h3>${values[0]}</h3><p class="user">${values[1]} | ${values[2]} | montant: <b class="final">${values[3]}</b></p><hr>`;
   listing.appendChild(newTodo);
 }
 
@@ -47,12 +52,12 @@ function loadTodos() {
   }
 }
 
-function addTodoToLocalStorage(users, tache, heure) {
+function addTodoToLocalStorage(users, tache, heure, montant) {
   const uid = new ShortUniqueId({ length: 10 });
   const uniqueId = uid.rnd();
   console.log(uniqueId);
-  localStorage.setItem(uniqueId, `${users},${tache},${heure}`);
-  createTodoElement(uniqueId, `${users},${tache},${heure}`);
+  localStorage.setItem(uniqueId, `${users},${tache},${heure},${montant}`);
+  createTodoElement(uniqueId, `${users},${tache},${heure},${montant}`);
 }
 
 function check() {
@@ -91,7 +96,21 @@ let btn = document.querySelector(".btn");
 if (btn) {
   btn.addEventListener("click", async function () {
     await addRandomTask();
-    addTodoToLocalStorage(users, tache, heure);
+    addTodoToLocalStorage(users, tache, heure, montant);
     check();
   });
 }
+
+btnTotal.addEventListener("click", () => {
+  let m = 0,
+    n = 0;
+
+  let montantFinal = document.querySelectorAll(".final");
+  console.log(montantFinal);
+  for (let i = 0; i < montantFinal.length; i++) {
+    m = parseFloat(montantFinal[i].innerText).toFixed(2);
+    n = parseFloat(n) + parseFloat(m);
+    console.log(n);
+  }
+  score.innerHTML = n + "€";
+});
